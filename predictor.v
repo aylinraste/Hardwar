937d4fba4a0c6a1710d13cd8f777c3fc7c597c78
Theborna <borna710@gmail.com>
2023-06-01 10:39:08 +0330

    wadsadkasdoi

--- predictor.v
+++ predictor.v
@@ -9,35 +9,35 @@ module predictor (
   reg [1:0] y = 0;
   parameter [1:0] STRONG_TAKEN = 3, WEAK_TAKEN = 2, WEAK_NOT_TAKEN = 1, STRONG_NOT_TAKEN = 0;
 
-  always @(posedge clk or result or request) begin
+  always @(posedge clk) begin
     if (result) begin
-      //   case ({
-      //     taken, y
-      //   })
-      // {1, STRONG_TAKEN} : y <= STRONG_TAKEN;
-      // {0, STRONG_TAKEN} : y <= WEAK_TAKEN;
-      // {1, WEAK_TAKEN} : y <= STRONG_TAKEN;
-      // {0, WEAK_TAKEN} : y <= WEAK_NOT_TAKEN;
-      // {1, WEAK_NOT_TAKEN} : y <= WEAK_TAKEN;
-      // {0, WEAK_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
-      // {1, STRONG_NOT_TAKEN} : y <= WEAK_NOT_TAKEN;
-      // {0, STRONG_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
+    //   case ({
+    //     taken, y
+    //   })
+    //     {1, STRONG_TAKEN} : y <= STRONG_TAKEN;
+    //     {0, STRONG_TAKEN} : y <= WEAK_TAKEN;
+    //     {1, WEAK_TAKEN} : y <= STRONG_TAKEN;
+    //     {0, WEAK_TAKEN} : y <= WEAK_NOT_TAKEN;
+    //     {1, WEAK_NOT_TAKEN} : y <= WEAK_TAKEN;
+    //     {0, WEAK_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
+    //     {1, STRONG_NOT_TAKEN} : y <= WEAK_NOT_TAKEN;
+    //     {0, STRONG_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
 
-      //     default: y <= 0;
-      //   endcase
-      case ({
-        taken[0], y[1:0]
-      })
-        {1'b1, 2'b11} : y <= STRONG_TAKEN;
-        {1'b0, 2'b11} : y <= WEAK_TAKEN;
-        {1'b1, 2'b10} : y <= STRONG_TAKEN;
-        {1'b0, 2'b10} : y <= WEAK_NOT_TAKEN;
-        {1'b1, 2'b01} : y <= WEAK_TAKEN;
-        {1'b0, 2'b01} : y <= STRONG_NOT_TAKEN;
-        {1'b1, 2'b00} : y <= WEAK_NOT_TAKEN;
-        {1'b0, 2'b00} : y <= STRONG_NOT_TAKEN;
-        default: y = 2'b00;
-      endcase
+    //     default: y <= 0;
+    //   endcase
+        case ({
+          taken, y[1:0]
+        })
+          {1'b1, 2'b11} : y <= STRONG_TAKEN;
+          {1'b0, 2'b11} : y <= WEAK_TAKEN;
+          {1'b1, 2'b10} : y <= STRONG_TAKEN;
+          {1'b0, 2'b10} : y <= WEAK_NOT_TAKEN;
+          {1'b1, 2'b01} : y <= WEAK_TAKEN;
+          {1'b0, 2'b01} : y <= STRONG_NOT_TAKEN;
+          {1'b1, 2'b00} : y <= WEAK_NOT_TAKEN;
+          {1'b0, 2'b00} : y <= STRONG_NOT_TAKEN;
+          default: y = 2'b00;
+        endcase
 
     end
     if (request) begin
