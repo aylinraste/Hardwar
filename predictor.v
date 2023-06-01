0f77c7e52a05bc6629cb8d050b89ce64f1d5da05
Theborna <borna710@gmail.com>
2023-06-01 10:29:27 +0330

    modified

--- predictor.v
+++ predictor.v
@@ -1,31 +1,32 @@
 module predictor (
     input  wire request,
-    result,
-    clk,
-    taken,
+    input  wire result,
+    input  wire clk,
+    input  wire taken,
     output reg  prediction
 );
 
-  reg [1:0] y;
+  reg [1:0] y = 0;
   parameter [2:0] STRONG_TAKEN = 3, WEAK_TAKEN = 2, WEAK_NOT_TAKEN = 1, STRONG_NOT_TAKEN = 0;
 
-  always @(posedge clk) begin
+  always @(posedge clk or result or request) begin
     if (result) begin
       case ({
         taken, y
       })
-        (1, STRONG_TAKEN): y <= STRONG_TAKEN;
-        (0, STRONG_TAKEN): y <= WEAK_TAKEN;
-        (1, WEAK_TAKEN): y <= STRONG_TAKEN;
-        (0, WEAK_TAKEN): y <= WEAK_NOT_TAKEN;
-        (1, WEAK_NOT_TAKEN): y <= WEAK_TAKEN;
-        (0, WEAK_NOT_TAKEN): y <= STRONG_NOT_TAKEN;
-        (1, STRONG_NOT_TAKEN): y <= WEAK_NOT_TAKEN;
-        (0, STRONG_NOT_TAKEN): y <= STRONG_NOT_TAKEN;
+        {1, STRONG_TAKEN} : y = STRONG_TAKEN;
+        {0, STRONG_TAKEN} : y = WEAK_TAKEN;
+        {1, WEAK_TAKEN} : y = STRONG_TAKEN;
+        {0, WEAK_TAKEN} : y = WEAK_NOT_TAKEN;
+        {1, WEAK_NOT_TAKEN} : y = WEAK_TAKEN;
+        {0, WEAK_NOT_TAKEN} : y = STRONG_NOT_TAKEN;
+        {1, STRONG_NOT_TAKEN} : y = WEAK_NOT_TAKEN;
+        {0, STRONG_NOT_TAKEN} : y = STRONG_NOT_TAKEN;
+        default: y = 0;
       endcase
     end
     if (request) begin
-      prediction <= y[0];
+      prediction = y[0];
     end
   end
 
