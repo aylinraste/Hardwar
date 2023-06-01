c5ec194cca567922ff09a5dcca338b7c9accb7af
Theborna <borna710@gmail.com>
2023-06-01 10:36:29 +0330

    sam

--- predictor.v
+++ predictor.v
@@ -11,19 +11,34 @@ module predictor (
 
   always @(posedge clk or result or request) begin
     if (result) begin
+      //   case ({
+      //     taken, y
+      //   })
+      // {1, STRONG_TAKEN} : y <= STRONG_TAKEN;
+      // {0, STRONG_TAKEN} : y <= WEAK_TAKEN;
+      // {1, WEAK_TAKEN} : y <= STRONG_TAKEN;
+      // {0, WEAK_TAKEN} : y <= WEAK_NOT_TAKEN;
+      // {1, WEAK_NOT_TAKEN} : y <= WEAK_TAKEN;
+      // {0, WEAK_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
+      // {1, STRONG_NOT_TAKEN} : y <= WEAK_NOT_TAKEN;
+      // {0, STRONG_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
+
+      //     default: y <= 0;
+      //   endcase
       case ({
-        taken, y
+        taken[0], y[1:0]
       })
-        {1, STRONG_TAKEN} : y <= STRONG_TAKEN;
-        {0, STRONG_TAKEN} : y <= WEAK_TAKEN;
-        {1, WEAK_TAKEN} : y <= STRONG_TAKEN;
-        {0, WEAK_TAKEN} : y <= WEAK_NOT_TAKEN;
-        {1, WEAK_NOT_TAKEN} : y <= WEAK_TAKEN;
-        {0, WEAK_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
-        {1, STRONG_NOT_TAKEN} : y <= WEAK_NOT_TAKEN;
-        {0, STRONG_NOT_TAKEN} : y <= STRONG_NOT_TAKEN;
-        default: y <= 0;
+        {1'b1, 2'b11} : y <= STRONG_TAKEN;
+        {1'b0, 2'b11} : y <= WEAK_TAKEN;
+        {1'b1, 2'b10} : y <= STRONG_TAKEN;
+        {1'b0, 2'b10} : y <= WEAK_NOT_TAKEN;
+        {1'b1, 2'b01} : y <= WEAK_TAKEN;
+        {1'b0, 2'b01} : y <= STRONG_NOT_TAKEN;
+        {1'b1, 2'b00} : y <= WEAK_NOT_TAKEN;
+        {1'b0, 2'b00} : y <= STRONG_NOT_TAKEN;
+        default: y = 2'b00;
       endcase
+
     end
     if (request) begin
       prediction <= y[0];
